e1fc41728d53fd9d0b3af225e662c10afb801488
Beast Code <beastcode9999@gmail.com>
2024-03-28 14:10:08 +0930

    add css for .card elements

--- script.js
+++ script.js
@@ -8,50 +8,42 @@
 // Add ingredients
 const Ingredients = (function Ingredients() {
     function getElemById(id) {
+        return document.getElementById(id);
+    }
+
+    _ingredientNum = 1;
+
+    function getElemById(id) {
 		return document.getElementById(id);
 	}
     const _container = getElemById('ingredients-container');
-    const _add = getElemById('add');
-
+    
     const _addDiv = function _addDiv() {
+        const _add = getElemById('add');
         _add.addEventListener('click', ()=> {
+            _ingredientNum++;
             let div = document.createElement("div");
             div.classList.add('form__section__div', 'margin-bottom-small')
-            div.innerHTML = `<input type="text" placeholder="Ingredient 2"> <a href="#" class="remove" id="remove">Remove</a>`
+            div.innerHTML = `<input type="text" placeholder="Ingredient ${_ingredientNum}"> <a href="#" class="remove" id="remove">Remove</a>`
             _container.appendChild(div)
         })
     };
-
     const _removeDiv = function _removeDiv() {
-        function getElemById(id) {
-            return document.getElementById(id);
-        }
-
-        let _btn = getElemById('remove');
-
-
         window.addEventListener('click', (e)=> {
             const target = e.target
 
-            console.log(target.id)
-
             if( target.id == "remove" ) {
+                _ingredientNum--;
                 target.parentElement.remove()
-
             }
         })
-    }
-    // <div class="form__section__div margin-bottom-small">
-    //     <input type="text" placeholder="Ingredient 2">
-    //     <a href="#" class="remove" id="remove">Remove</a>
-    // </div>  
+    }  
 
+    
     const updateContainer = function updateContainer() {
         _addDiv()
         _removeDiv()
     };
-
-
 
     const init = function init() {
         updateContainer();
