7888b0300d92e5e02fd73551e9d6a69acb514f9d
Beast Code <beastcode9999@gmail.com>
2024-03-28 12:40:53 +0930

    add _removeDiv()

--- script.js
+++ script.js
@@ -11,22 +11,36 @@ const Ingredients = (function Ingredients() {
 		return document.getElementById(id);
 	}
     const _container = getElemById('ingredients-container');
-
     const _add = getElemById('add');
-
 
     const _addDiv = function _addDiv() {
         _add.addEventListener('click', ()=> {
             let div = document.createElement("div");
-
             div.classList.add('form__section__div', 'margin-bottom-small')
-
-            div.innerHTML = `<input type="text" placeholder="Ingredient 2">
-              <a href="#" class="remove" id="remove">Remove</a>`
+            div.innerHTML = `<input type="text" placeholder="Ingredient 2"> <a href="#" class="remove" id="remove">Remove</a>`
             _container.appendChild(div)
         })
     };
 
+    const _removeDiv = function _removeDiv() {
+        function getElemById(id) {
+            return document.getElementById(id);
+        }
+
+        let _btn = getElemById('remove');
+
+
+        window.addEventListener('click', (e)=> {
+            const target = e.target
+
+            console.log(target.id)
+
+            if( target.id == "remove" ) {
+                target.parentElement.remove()
+
+            }
+        })
+    }
     // <div class="form__section__div margin-bottom-small">
     //     <input type="text" placeholder="Ingredient 2">
     //     <a href="#" class="remove" id="remove">Remove</a>
@@ -34,8 +48,7 @@ const Ingredients = (function Ingredients() {
 
     const updateContainer = function updateContainer() {
         _addDiv()
-
-
+        _removeDiv()
     };
 
 
